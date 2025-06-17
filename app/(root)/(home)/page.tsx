4838715db3fef17345bd362c5558aeee3ec2d72e
ChinmayNoob <chinmaysk0408@gmail.com>
2025-06-17 19:03:34 +0530

    feat: building home page and creating question list

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { Metadata } from "next";
+import Link from "next/link";
+import QuestionsList from "@/components/shared/QuestionsList";
 
-const page = () => {
+export const metadata: Metadata = {
+    title: "Home",
+    description: "Home page",
+};
+
+export default async function Home() {
+    const { userId } = await auth();
+
     return (
-        <div>Questions related to the Data Structure and Algorithms</div>
-    )
-}
+        <div className="px-2 sm:px-12 mt-4">
+            <div className="flex w-full flex-col justify-between gap-4 sm:flex-row sm:items-center">
+                <h1 className="font-bold text-2xl">All Questions</h1>
 
-export default page
+                <Link href={`/ask-question`} className="flex justify-end max-sm:w-full">
+                    <Button className="px-4 py-2">
+                        Ask a Question
+                    </Button>
+                </Link>
+            </div>
+            <QuestionsList
+                params={{
+                    page: 1,
+                    pageSize: 10,
+                }}
+                clerkId={userId}
+            />
+        </div>
+    );
+}
